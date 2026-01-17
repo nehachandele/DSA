98ad3b052c2028bef41205aea75b6e44022cf406
Neha Chandele <nehachandele2211@gmail.com>
2026-01-17 11:33:27 +0530

    Prime Number check

---
+++ PrimeNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int num=sc.nextInt();
+        int count=0;
+        for(int i=1;i<=Math.sqrt(num);i++){
+            if(num%i==0){
+                count++;
+                if(i!=num/i){
+                    count++;
+                }
+            }
+        }
+        if(count==2){
+            System.out.println(num + " is a Prime Number");
+        } else {
+            System.out.println(num + " is not a Prime Number");
+    }
+}
+}
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Platform for DSA Practice and Problem solving
 # Pronlem 3 Check if the Number is Palindrome
 # Problem 4 Armstrong Number
 # Problem 5 print all Divisions of certain number
+# Problem 5 
