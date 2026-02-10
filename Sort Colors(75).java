56f9c6f6298d4a7b4076a6564fa1f29646ef60e2
Neha Chandele <nehachandele2211@gmail.com>
2026-02-10 14:14:15 +0530

    Solved 75

---
+++ Sort Colors(75).java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left=0;
+        int mid=0;
+        int right=nums.length-1;
+        while(mid<=right){
+            if(nums[mid]==0){
+                swap(nums,left,mid);
+                left++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums,right,mid);
+                right--;
+            }
+
+        }
+    }
+    public void swap(int[] nums,int i,int j){
+        int k=nums[i];
+        nums[i]=nums[j];
+        nums[j]=k;
+    }
+}
