4d30eb22a49b619f664b95f682e3951ba8d62b1c
Neha Chandele <nehachandele2211@gmail.com>
2026-02-11 10:21:10 +0530

    198 solved

---
+++ House Robber(198).java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prevRob=0;
+        int maxRob=0;
+
+        for(int curValue:nums){
+            int temp=Math.max(maxRob,curValue+prevRob);
+            prevRob=maxRob;
+            maxRob=temp;
+        }
+        return maxRob;
+    }
+}
